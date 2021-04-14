caa162597db0ee634f675aac6f3fb0dc67ab6674
Travis Courtney <travis@angry-fly.com>
2021-04-14 15:12:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const writeCards = function (arr, occasion) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(`Thank you, ${arr[i]}, for the wonderful ${occasion} gift!`);
+    console.log(i);
+  }
+  return newArray;
+}
+
+let countDown = function (highNum) {
+  let counter = highNum;
+  while (counter >= 0 )  {
+    console.log(counter)
+    counter--
+  }
+}
+
